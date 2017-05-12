66824322d41bbdc59129ab054acc55a921b85f69
Lara Zhuga <larazhuga88888@gmail.com>
2017-05-12 17:27:39 +0300

    form

--- views/site/index.php
+++ views/site/index.php
@@ -13,6 +13,77 @@ $urlMockup = '/images/mockup/';
             <!--            <h1>Left page title version</h1>-->
             <!--            <span>Subtext for page title. Lorem ipsum viverra a!</span>-->
         </div>
+        <div class="form-relative clearfix">
+            <div class="form-group form-group-sm">
+                <form id="sign-form" action="/sign-up" method="post" enctype="multipart/form-data">
+                  
+                    <div class="form-group btn-group btn-group-justified" role="group" aria-label="...">
+                      <div class="btn-group" role="group">
+                        <a href="" class="social-facebook btn-sm btn"><i class="fa fa-facebook"></i> facebook</a>
+                      </div>
+                      <div class="btn-group" role="group">
+                        <a href="" class="social-twitter btn-sm btn"><i class="fa fa-twitter"></i> twitter</a>
+                      </div>
+                      <div class="btn-group" role="group">
+                        <a href="" class="social-google btn-sm btn"><i class="fa fa-google-plus"></i> google</a>
+                      </div>
+                    </div>                         
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                        
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                   
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                        
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                   
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                        
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                   
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                        
+                        <div class="col-md-6">
+                            <input id="" name="" placeholder="First Name" aria-required="true" class="form-control" type="text"><p class="help-block help-block-error"></p>
+                        </div>                   
+                    </div>
+
+                    <div class="row">
+                        <div class="form-group">                                       
+                    <label class="sr-only"><label class="control-label" for="registrationform-recaptcha">Re Captcha</label></label><input id="registrationform-recaptcha" name="RegistrationForm[reCaptcha]" type="hidden"><div class="g-recaptcha" data-sitekey="6LcfERsUAAAAAF9HF7Ay5PA3l6DqM4Muj_5DmxN1" data-callback="recaptchaCallback" data-expired-callback="recaptchaExpiredCallback"><div style="width: 304px; height: 78px;"><div><iframe src="https://www.google.com/recaptcha/api2/anchor?k=6LcfERsUAAAAAF9HF7Ay5PA3l6DqM4Muj_5DmxN1&amp;co=aHR0cDovL3Jld2FyZGJ1Y2tzLnNmZGV2c2VydmVyLmNvbTo4MA..&amp;hl=en&amp;v=r20170503135251&amp;size=normal&amp;cb=cqrne3ufakbs" title="recaptcha widget" scrolling="no" name="undefined" height="78" frameborder="0" width="304"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none;  display: none; "></textarea></div></div><p class="help-block help-block-error"></p>
+                                           
+                            </div>
+
+                    </div>
+
+                    <div class="text-left">
+                        <button type="submit" class="btn btn-primary btn-sm">Register New Account</button>                  
+                    </div>
+
+                    <div class="text-left ">
+                        <a href="">Already have an account? </a>
+                    </div>
+
+                </form>
+            </div>    
+        </div>
     </div>
 </section>
 <!-- END: PAGE TITLE -->
