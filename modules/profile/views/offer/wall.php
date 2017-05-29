d3eb2b2af549d0e8e9ecbff6369514f9805a0af5
Denis Shlyakhov <21Denis1991@gmail.com>
2017-05-29 13:02:54 +0300

    Update  offerwalls categories (mock)

--- modules/profile/views/offer/wall.php
+++ modules/profile/views/offer/wall.php
@@ -21,8 +21,13 @@ $this->title = 'Offer Walls';
     <!-- END: Portfolio Filter -->
 
     <!-- Portfolio Items -->
-    <div id="isotope" class="isotope portfolio-items" data-isotope-item-space="0" data-isotope-mode="masonry" data-isotope-col="4" data-isotope-item=".portfolio-item" style="position: relative; height: 100%; margin-right: 0%;">
-        <div class="col-md-3 portfolio-item">
+    <div id="isotope" class="isotope portfolio-items"
+         data-isotope-item-space="3"
+         data-isotope-mode="fitRows"
+         data-isotope-col="4"
+         data-isotope-item=".portfolio-item"
+         style="position: relative; height: 100%; margin-right: 0%;">
+        <div class="portfolio-item">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/adwork-media.png', ['alt' => 'adwork-media', 'class' => 'timeline-badge-userpic']),
@@ -31,7 +36,7 @@ $this->title = 'Offer Walls';
             </div>
         </div>
 
-        <div class="col-md-3 portfolio-item cat3 cat4">
+        <div class="portfolio-item cat3 cat4">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/offertoro.png', ['alt' => 'offer-toro', 'class' => 'timeline-badge-userpic']),
@@ -39,7 +44,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat2 cat3 cat4">
+        <div class="portfolio-item cat2 cat3 cat4">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/clixwall.png', ['alt' => 'clixwall', 'class' => 'timeline-badge-userpic']),
@@ -47,7 +52,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat1 cat5">
+        <div class="portfolio-item cat1 cat5">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/offerdaddy.png', ['alt' => 'offer-daddy', 'class' => 'timeline-badge-userpic']),
@@ -55,7 +60,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="portfolio-item col-md-3 cat2 cat4">
+        <div class="portfolio-item cat2 cat4">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/ptcwall.jpg', ['alt' => 'ptcwall', 'class' => 'timeline-badge-userpic']),
@@ -63,7 +68,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat4 cat5">
+        <div class="portfolio-item cat4 cat5">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/kiwiwall.png', ['alt' => 'kiwiwall', 'class' => 'timeline-badge-userpic']),
@@ -71,7 +76,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat1 cat3">
+        <div class="portfolio-item cat1 cat3">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/superrewards.png', ['alt' => 'superrewards', 'class' => 'timeline-badge-userpic']),
@@ -79,7 +84,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat3 ca5">
+        <div class="portfolio-item cat3 ca5">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/minutestaff.png', ['alt' => 'minutestaff', 'class' => 'timeline-badge-userpic']),
@@ -87,7 +92,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat1 cat2">
+        <div class="portfolio-item cat1 cat2">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/cpalead.png', ['alt' => 'cpalead', 'class' => 'timeline-badge-userpic']),
@@ -95,7 +100,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat3 cat4 cat5">
+        <div class="portfolio-item cat3 cat4 cat5">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/persona.jpg', ['alt' => 'persona', 'class' => 'timeline-badge-userpic']),
@@ -103,7 +108,7 @@ $this->title = 'Offer Walls';
                 ); ?>
             </div>
         </div>
-        <div class="col-md-3 portfolio-item cat1 cat2">
+        <div class="portfolio-item cat1 cat2">
             <div class="timeline-img">
                 <?= Html::a(
                     Html::img('/images/offer-providers/fyber_logo.png', ['alt' => 'fyber', 'class' => 'timeline-badge-userpic']),
