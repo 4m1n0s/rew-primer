a1e82412a06b7240b46262d8dbb5b149cd5d69d0
Denis Shlyakhov <21denis1991@gmail.com>
2017-06-26 15:46:56 +0300

    Update suspicious activity message

--- modules/profile/views/offer/deny.php
+++ modules/profile/views/offer/deny.php
@@ -2,5 +2,5 @@
 /* @var \yii\web\View $this */
 ?>
 
-<p>We have detected suspicious activity on your account. If you think, your content was locked by mistake,
-    <?php echo \yii\helpers\Html::a('contact with us', ['/contact/index/index'])?></p>
+<p>We have detected suspicious activity on your account. If You think your content was locked by mistake,
+    <?php echo \yii\helpers\Html::a('contact us', ['/contact/index/index'])?></p>
