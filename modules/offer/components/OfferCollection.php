66199f70bd1b6050debf5475f28e76af4c721ba9
Denis Shlyakhov <21Denis1991@gmail.com>
2017-06-26 22:00:18 +0300

    Fix OfferCollection::append signature

--- modules/offer/components/OfferCollection.php
+++ modules/offer/components/OfferCollection.php
@@ -3,11 +3,16 @@
 namespace app\modules\offer\components;
 
 use app\modules\offer\models\Offer;
+use yii\base\InvalidConfigException;
 
 class OfferCollection extends \ArrayIterator
 {
-    public function append(Offer $value)
+    public function append($value)
     {
+        if (!$value instanceof Offer) {
+            throw new InvalidConfigException(__METHOD__ . ' method must accept ' . Offer::class . ' object only');
+        }
+
         parent::append($value);
     }
 }
