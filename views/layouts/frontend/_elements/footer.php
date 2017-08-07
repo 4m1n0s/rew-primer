08e887b43473539ab1bd1c0841cdb7b37abaaaad
fomichev <w.dmitrij.fomichev@gmail.com>
2017-08-07 12:44:43 +0300

    Social links target blank

--- views/layouts/frontend/_elements/footer.php
+++ views/layouts/frontend/_elements/footer.php
@@ -32,14 +32,14 @@
                 <div class="col-md-6">
                     <div class="social-icons">
                         <ul>
-                            <li class="social-facebook"><a href="<?= Yii::$app->keyStorage->get('social.fb', '#') ?>"><i class="fa fa-facebook"></i></a></li>
-                            <li class="social-twitter"><a href="<?= Yii::$app->keyStorage->get('social.twitter', '#') ?>"><i class="fa fa-twitter"></i></a></li>
-                            <li class="social-google"><a href="<?= Yii::$app->keyStorage->get('social.google', '#') ?>"><i class="fa fa-google-plus"></i></a></li>
-                            <li class="social-youtube"><a href="<?= Yii::$app->keyStorage->get('social.youtube', '#') ?>"><i class="fa fa-youtube-play"></i></a></li>
+                            <li class="social-facebook" ><a href="<?= Yii::$app->keyStorage->get('social.fb', '#') ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li class="social-twitter"><a href="<?= Yii::$app->keyStorage->get('social.twitter', '#') ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li class="social-google"><a href="<?= Yii::$app->keyStorage->get('social.google', '#') ?>" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                            <li class="social-youtube"><a href="<?= Yii::$app->keyStorage->get('social.youtube', '#') ?>" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
 <!--                             <li class="social-pinterest"><a href="#"><i class="fa fa-pinterest"></i></a></li>
                             <li class="social-vimeo"><a href="#"><i class="fa fa-vimeo-square"></i></a></li>
                             <li class="social-linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li class="social-dribbble"><a href="#"><i class="fa fa-dribbble"></i></a></li> 
+                            <li class="social-dribbble"><a href="#"><i class="fa fa-dribbble"></i></a></li>
                             <li class="social-youtube"><a href="#"><i class="fa fa-youtube-play"></i></a></li>
                             <li class="social-rss"><a href="#"><i class="fa fa-rss"></i></a></li>-->
                         </ul>
