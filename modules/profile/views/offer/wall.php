78ed6d6c6c06dea85476e239f215137656872365
Denis Shlyakhov <21denis1991@gmail.com>
2017-08-21 19:04:43 +0300

    Update offerwall styles

--- modules/profile/views/offer/wall.php
+++ modules/profile/views/offer/wall.php
@@ -12,7 +12,7 @@ $this->title = 'Offer Walls';
     <img width="40" src="/images/svg-loaders/bars.svg" alt="">
     <div class="m-t-100"></div>
 </div>
-<div id="offer-walls-wrap" style="display: none">
+<div id="offer-walls-wrap" style="opacity: 0">
     <!--Offer Filter-->
     <ul class="portfolio-filter clearfix" id="portfolio-filter" data-isotope-nav="isotope">
         <li class="ptf-active" data-filter="*">Show All</li>
@@ -51,7 +51,7 @@ $this->title = 'Offer Walls';
 
 <?php
 $js = <<< JS
-    $('#offer-walls-wrap').show();
+    $('#offer-walls-wrap').css('opacity', '1');
     $('.loader').addClass('hide');
 JS;
 
