c4381a25e51b3accb57ce0353116b7475ffeade6
Denis Shlyakhov <21denis1991@gmail.com>
2017-08-21 10:48:44 +0300

    Fix amazon client

--- modules/core/components/mailer/clients/Amazon.php
+++ modules/core/components/mailer/clients/Amazon.php
@@ -26,7 +26,7 @@ class Amazon implements MailerInterface
         $request['Source'] = $emailQueue->sender;
         $request['Destination']['ToAddresses'] = [$emailQueue->recipient];
         $request['Message']['Subject']['Data'] = $emailQueue->template->subject;
-        $request['Message']['Body']['Text']['Data'] = $this->compose($emailQueue->template, Json::decode($emailQueue->params));
+        $request['Message']['Body']['Html']['Data'] = $this->compose($emailQueue->template, Json::decode($emailQueue->params));
 
         try {
             $result = $client->sendEmail($request);
