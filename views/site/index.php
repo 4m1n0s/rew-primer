9de3fb3287e24968be9b725cfb95ead25f9c84b3
Lara Zhuga <larazhuga88888@gmail.com>
2017-08-21 14:13:40 +0300

    txt

--- views/site/index.php
+++ views/site/index.php
@@ -44,21 +44,18 @@ $urlMockup = '/images/mockup/';
                 <div class="icon-box effect medium center border">
                     <div class="icon"> <a href="/about#section-1"><i class="fa fa-check-square"></i></a> </div>
                     <h3>Completing surveys</h3>
-                    <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12">
                 <div class="icon-box effect medium center border">
                     <div class="icon"> <a href="/about#section-2"><i class="fa fa-video-camera"></i></a> </div>
                     <h3>Watching videos</h3>
-                    <p>Suspendisse condimentum porttitor cursumus. Lorem ipsum dolor sit amet, consecte.</p>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12">
                 <div class="icon-box effect medium center border">
                     <div class="icon"> <a href="/about#section-3"><i class="fa fa-search"></i></a> </div>
                     <h3>Searching the web</h3>
-                    <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
                 </div>
             </div>
         </div>
@@ -68,15 +65,6 @@ $urlMockup = '/images/mockup/';
 
 <section>
     <div class="container">
-        <div class="text-left">
-            <h2>Join RewardBucks - Make "Bucks" Now.</h2>
-        </div>
-        <br>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam.</p>
-            </div>
-        </div>
         <div class="text-center">
             <a class="button btn-primary button-3d effect fill" href="/sign-up"><span>Sign up & get "Bucks" today</span></a>
         </div>
